bd007ef0e7bb1e591553fe8555c176227f4cf004
SOHAN KUMAR CHOUDHARY <sohans1993@gmail.com>
2018-02-27 19:41:27 -0800

    indexer fileio program

---
+++ Class1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication3
+{
+    class Class1
+    {
+
+
+        internal static void Testing()
+        {
+            log4net.Config.BasicConfigurator.Configure();
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+            try
+            {
+                log.Info("program for writing and reading from file");
+                log.Error("error message");
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error Message: " + ex.Message.ToString(), ex);
+            }
+        }
+        string file1 = "e:\\w.txt";
+       
+        StringWriter sw = new StringWriter();
+
+
+        public void writef()
+        {
+
+            using (BinaryWriter bw = new BinaryWriter(File.Open(file1, FileMode.Create)))
+            {
+                bw.Write("hello");
+                bw.Write(100);
+                bw.Write(true);
+            }
+
+            // stringwriter
+
+
+            sw.WriteLine("basics of c programing");
+
+            sw.Close();
+            // fileInfo
+            try
+            {
+                FileInfo f1 = new FileInfo("e:\\abc.txt");
+                StreamWriter s1 = f1.CreateText();
+                s1.WriteLine("welcome to india");
+                s1.Close();
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("SOME THING WENT WRONG " + e);
+            }
+
+        }
+
+        public void readf()
+        {
+            using (BinaryReader br = new BinaryReader(File.Open(file1, FileMode.Open)))
+            {
+                Console.WriteLine(br.ReadString());
+                Console.WriteLine(br.ReadInt32());
+                Console.WriteLine(br.ReadBoolean());
+            }
+            StringReader sr = new StringReader(sw.ToString());
+            while (sr.Peek() > -1)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            // reading in fileInfo
+            try
+            {
+                FileInfo f2 = new FileInfo("e:\\abc.txt");
+                StreamReader str = f2.OpenText();
+                string data = "";
+                while ((data = str.ReadLine()) != null)
+                {
+                    Console.WriteLine("Fileinfo " + data);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("error" + e);
+            }
+
+        }
+    }
+  class A
+        {
+            public static void Main(string[] args)
+            {
+            Class1 ob = new Class1();
+            ob.writef();
+            ob.readf();
+            Console.Read();
+            Console.WriteLine("hello");
+            Class1.Testing();
+            Console.ReadLine();
+
+        }
+        }
+    }
+
---
+++ Emp.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Emp
+    {
+
+        internal static void Testing()
+        {
+            log4net.Config.BasicConfigurator.Configure();
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+            try
+            {
+                log.Info("program for writing and reading from file");
+                log.Error("error message");
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error Message: " + ex.Message.ToString(), ex);
+            }
+        }
+        String name;
+        int age;
+        int salary;
+
+        string dept;
+
+        public Emp(int age, string name, int salary, string dept)
+        {
+            this.age = age;
+            this.name = name;
+            this.salary = salary;
+            this.dept = dept;
+        }
+        public object this[int i]
+        {
+            get
+            {
+                if (i == 1)
+                    return age;
+                else if (i == 2)
+                    return name;
+                else if (i == 3)
+                    return salary;
+                else if (i == 4)
+                    return dept;
+                return null;
+
+            }
+            set
+            {
+                if (i == 3)
+                    salary = (int)value;
+                if (i == 4)
+                    dept = (string)value;
+
+            }
+        }
+
+        class D
+        {
+            public static void Main(string[] args)
+            {
+                Emp e1 = new Emp(24, "ayush", 50000, "it engineer");
+                Console.WriteLine("detais of employee");
+                Console.WriteLine("name " + e1[1]);
+                Console.WriteLine("age " + e1[2]);
+                Console.WriteLine("salary " + e1[3]);
+                Console.WriteLine("dept " + e1[4]);
+
+                e1[3] = 40000;
+                e1[4] = "assistant";
+                Console.WriteLine("detais of employee after change");
+                Console.WriteLine("detais of employee ");
+                Console.WriteLine("name " + e1[1]);
+                Console.WriteLine("age " + e1[2]);
+                Console.WriteLine("salary " + e1[3]);
+                Console.WriteLine("dept " + e1[4]);
+                Class1.Testing();
+                Console.ReadLine();
+
+                Console.Read();
+
+
+            }
+        }
+
+    }
+}
+
+
+   
+
