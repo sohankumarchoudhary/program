4dcf7472eb770ceecfc2f026700bbf69e77c895f
SOHAN KUMAR CHOUDHARY <sohans1993@gmail.com>
2018-03-03 19:11:12 -0800

    delegate and async program

---
+++ Asyncprogram.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{ /// <summary>
+/// class for Asyncnchronous programing
+/// </summary>
+    class Asyncprogram
+    {
+        log4net.ILog log = log4net.LogManager.GetLogger(typeof(Asyncprogram));
+        
+        static Asyncprogram obj;
+
+        /// <summary>
+        /// it call Method1() and Method2()
+        /// </summary>
+        public  static  async void Method()
+        {
+            Task<int> task = Method1();
+            Method2();
+            int count = await task;
+            Method3(count);
+           
+
+        }
+
+        /// <summary>
+        /// it contain Task
+        /// and return count
+        /// </summary>
+        /// <returns></returns>
+
+
+        public static async Task<int> Method1()
+        {
+           
+            int count = 0;
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+
+                    obj.log.Info("method1");
+                    count += 1;
+                }
+            });
+          
+
+            return count;
+            
+            }
+
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                obj.log.Info(" Method 2");
+               
+            }
+        }
+
+
+        public static void Method3(int count)
+        {
+            obj.log.Info("Total count is " + count);
+            
+        }
+        static void Main(string[] args)
+        {
+
+            log4net.Config.BasicConfigurator.Configure();
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(Asyncprogram));
+
+            obj = new Asyncprogram();
+
+            Method();
+            Console.Read();
+            
+        }
+
+
+    }
+
+}
---
+++ ClsDelegate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{/// <summary>
+/// class showing delegate and event use
+/// </summary>
+    class ClsDelegate
+    {  /// <summary>
+       /// delegate defination
+       /// </summary>
+       /// <param name="a"></param>
+       /// <param name="b"></param>
+        public delegate void Delegatefun(int a, int b);
+        /// <summary>
+        /// event declaration
+        /// </summary>
+        public static event Delegatefun ev1;
+        log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+        /// <summary>
+        /// Aaa() to add
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void Add(int a, int b)
+        {
+            log.Info("sum is " + (a + b));
+        }
+        /// <summary>
+        /// mul() to multiply a and b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void mul(int a, int b)
+        {
+            log.Info("product is " + (a * b));
+        }
+
+
+        public static void Main(String[] args)
+        {
+            log4net.Config.BasicConfigurator.Configure();
+            log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
+            ClsDelegate ob = new ClsDelegate();
+            //  Delegatefun objd = new Delegatefun(ob.Add);
+            ev1 += new Delegatefun(ob.Add);
+            ev1 += new Delegatefun(ob.mul);
+            ev1(3, 4);
+            Console.Read();
+        }
+
+    }
+}
