417852dfc376fae4798a71cf01cb86ed6af8ba13
SOHAN KUMAR CHOUDHARY <sohans1993@gmail.com>
2018-03-09 19:54:20 -0800

    tcp/ip program html prog

---
+++ serv.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+
+    using System;
+    using System.Text;
+    using System.Net;
+    using System.Net.Sockets;
+
+    public class serv
+    {
+        public static void Main()
+        {
+            try
+            {
+                IPAddress ipAd = IPAddress.Parse("192.168.1.34");
+                // use local m/c IP address, and 
+                // use the same in the client
+
+                /* Initializes the Listener */
+                TcpListener myList = new TcpListener(ipAd, 8001);
+
+                /* Start Listeneting at the specified port */
+                myList.Start();
+
+                Console.WriteLine("The server is running at port 8001...");
+                Console.WriteLine("The local End point is  :" +
+                                  myList.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection.....");
+
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                byte[] b = new byte[100];
+                int k = s.Receive(b);
+                Console.WriteLine("Recieved...");
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(b[i]));
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                s.Send(asen.GetBytes("The string was recieved by the server."));
+                Console.WriteLine("\nSent Acknowledgement");
+                /* clean up */
+                s.Close();
+                myList.Stop();
+
+                Console.Read();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+            Console.Read();
+
+        }
+
+    }
+}
