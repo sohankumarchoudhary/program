c85415342da630754738b12d7258b6feb884eaad
SOHAN KUMAR CHOUDHARY <sohans1993@gmail.com>
2018-03-14 20:04:39 -0700

    javascript oops program and ajax progm

---
+++ JavaScript1.js
@@ -0,0 +1,21 @@
+﻿function emp(firstname , lastname )
+{
+    this.firstname = firstname;
+    this.lastname = lastname;
+    
+   
+}
+
+emp.prototype.getfullname= function()
+{
+   
+        return this.firstname  + this.lastname;
+    
+    
+}
+var otherdetail = function(age, id)
+{
+    this.age = age;
+    this.id = id;
+}
+
